08d9daf241a80f25d2b399c54e1010491474816e
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-02-07 19:17:20 -0600

    file creation and basic html/css

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//create wins, losses, correct, and incorrect variables
+
+//setTimeout for the quiz length
+
+//change html when submit or timer hits 0
+
+//display number of correct and incorrect guesses
